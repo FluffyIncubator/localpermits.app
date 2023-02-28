5e7efb6f7d164266d3790fbe361a24e5beb099b9
michaelfiber <michael+michaelfiber@cubeofb.org>
2023-02-27 23:28:27 -0500

    Add tsort to build

--- client/build.sh
+++ client/build.sh
@@ -5,20 +5,50 @@ set -o pipefail
 
 ###
 
+reIMPORT="import.*['\"](\./.*)['\"].*"
+declare -a DEPENDENCIES
+
 function copy {
 	cp -r ./src/static/* ./build/
 	printf "Copied..."
 }
 
-function embed {
-	content=""
-	sep=$'\n\n'
+function get_abs_path {
+	echo "$(cd "$(dirname $1)"; pwd)/$(basename "$1")"
+}
 
-	for file in $(find ./src -name "*.tsx"); do
-		content="$content$sep$(cat $file)"
+function build_dependencies {
+	local absolute_target="$(get_abs_path "$1")"
+
+	local line=""
+	while IFS= read -r line; do
+		if [[ $line =~ $reIMPORT ]]; then
+			absolute_dep=$(get_abs_path $(dirname $absolute_target)/${BASH_REMATCH[1]}.tsx)
+			DEPENDENCIES+=("$absolute_target $absolute_dep")
+			build_dependencies $absolute_dep
+		fi
+	done <<< "$(cat $absolute_target)"z
+}
+
+function getpairs {
+	for pair in "${DEPENDENCIES[@]}"; do
+		echo "$pair"
+	done
+}
+
+function embed {
+	local TSX=""
+	local SEP=$'\n\n'
+
+	local file=""
+	local pairs="$(getpairs)"
+	
+
+	for file in $(echo "$pairs" | tsort | tac); do
+		TSX="$TSX$SEP$(cat $file)"
 	done
 
-	template=$(cat ./src/index.html | CONTENT="$content" envsubst)
+	template=$(cat ./src/index.html | CONTENT="$TSX" envsubst)
 	printf "Embedded..."
 
 	echo "$template" > ./build/index.html
@@ -26,8 +56,17 @@ function embed {
 
 function update {
 	printf "\r                                            \r[%s] \e[1;33m" "$(date)"
+
+	printf "Copy..."
 	copy
+
+	DEPENDENCIES=()
+	printf "Build dependencies..."
+	build_dependencies $1
+
+	printf "Embed..."
 	embed
+
 	printf "Done\e[0m"
 }
 
@@ -35,11 +74,11 @@ function watch_for_changes {
 	old_md5=""
 	while :
 	do
-		md5=$(ls -lr --time-style=full-iso ./src/* | md5sum)
+		md5=$(ls -lR --time-style=full-iso $2 | md5sum)
 		if [ "$md5" != "$old_md5" ]; then
-			update
-			old_md5="$md5"
+			update $1
 		fi
+		old_md5="$md5"
 		sleep 1	
 	done
 }
@@ -48,7 +87,7 @@ function main {
 	mkdir -p ./build
 
 	echo "Start janky file watcher..."
-	watch_for_changes
+	watch_for_changes $1 $2
 }
 
-main
+main $1 $2
